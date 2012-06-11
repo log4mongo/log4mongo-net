59b07492eb90b58def076985c93c0391468824e1
andy rose <thatandyrose@gmail.com>
2012-06-11 20:13:24 +1000

    Added support for single Mongo Connection String
    The Appender exposes DB properties such as host, database, port etc.
    I've added the ConnectionString property, which if specified is used
    instead of the individual properties.

---
+++ src/Backup/log4mongo-net/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("log4mongo-net")]
+[assembly: AssemblyDescription("log4net appender for MongoDB database")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jozef Sevcik")]
+[assembly: AssemblyProduct("log4mongo-net")]
+[assembly: AssemblyCopyright("Copyright © Jozef Sevcik 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5514cb09-e403-48e8-be85-d0258c79d3d7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
