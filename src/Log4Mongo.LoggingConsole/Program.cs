21a5755f32a173251c29f1b3725959d0ebe66784
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2012-12-07 21:35:20 +0100

    fixes #10

---
+++ src/Log4Mongo.LoggingConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using log4net;
+using log4net.Config;
+using log4net.Util;
+
+namespace Log4Mongo.LoggingConsole
+{
+	public class Program
+	{
+		private static readonly TimeSpan Interval = TimeSpan.FromMilliseconds(100);
+		private static int _count;
+
+		public static void Main()
+		{
+			LogLog.InternalDebugging = true;
+			XmlConfigurator.Configure();
+
+			ILog log = LogManager.GetLogger(typeof(Program));
+			log.Info("Starting");
+			while (!Console.KeyAvailable)
+			{
+				log.Info(++_count);
+				Thread.Sleep(Interval);
+			}
+		}
+	}
+}
--- src/Log4Mongo/BackwardCompatibility.cs
+++ src/Log4Mongo/BackwardCompatibility.cs
@@ -32,29 +32,30 @@ namespace Log4Mongo
 				return null;
 			}
 
-			var toReturn = new BsonDocument();
-			toReturn["timestamp"] = loggingEvent.TimeStamp;
-			toReturn["level"] = loggingEvent.Level.ToString();
-			toReturn["thread"] = loggingEvent.ThreadName;
-			toReturn["userName"] = loggingEvent.UserName;
-			toReturn["message"] = loggingEvent.RenderedMessage;
-			toReturn["loggerName"] = loggingEvent.LoggerName;
-			toReturn["domain"] = loggingEvent.Domain;
-			toReturn["machineName"] = Environment.MachineName;
+			var toReturn = new BsonDocument {
+				{"timestamp", loggingEvent.TimeStamp}, 
+				{"level", loggingEvent.Level.ToString()}, 
+				{"thread", loggingEvent.ThreadName}, 
+				{"userName", loggingEvent.UserName}, 
+				{"message", loggingEvent.RenderedMessage}, 
+				{"loggerName", loggingEvent.LoggerName}, 
+				{"domain", loggingEvent.Domain}, 
+				{"machineName", Environment.MachineName}
+			};
 
 			// location information, if available
 			if(loggingEvent.LocationInformation != null)
 			{
-				toReturn["fileName"] = loggingEvent.LocationInformation.FileName;
-				toReturn["method"] = loggingEvent.LocationInformation.MethodName;
-				toReturn["lineNumber"] = loggingEvent.LocationInformation.LineNumber;
-				toReturn["className"] = loggingEvent.LocationInformation.ClassName;
+				toReturn.Add("fileName", loggingEvent.LocationInformation.FileName);
+				toReturn.Add("method", loggingEvent.LocationInformation.MethodName);
+				toReturn.Add("lineNumber", loggingEvent.LocationInformation.LineNumber);
+				toReturn.Add("className", loggingEvent.LocationInformation.ClassName);
 			}
 
 			// exception information
 			if(loggingEvent.ExceptionObject != null)
 			{
-				toReturn["exception"] = BuildExceptionBsonDocument(loggingEvent.ExceptionObject);
+				toReturn.Add("exception", BuildExceptionBsonDocument(loggingEvent.ExceptionObject));
 			}
 
 			// properties
@@ -64,10 +65,10 @@ namespace Log4Mongo
 				var properties = new BsonDocument();
 				foreach(DictionaryEntry entry in compositeProperties)
 				{
-					properties[entry.Key.ToString()] = entry.Value.ToString();
+					properties.Add(entry.Key.ToString(), entry.Value.ToString());
 				}
 
-				toReturn["properties"] = properties;
+				toReturn.Add("properties", properties);
 			}
 
 			return toReturn;
@@ -75,14 +76,15 @@ namespace Log4Mongo
 
 		private static BsonDocument BuildExceptionBsonDocument(Exception ex)
 		{
-			var toReturn = new BsonDocument();
-			toReturn["message"] = ex.Message;
-			toReturn["source"] = ex.Source ?? string.Empty;
-			toReturn["stackTrace"] = ex.StackTrace ?? string.Empty;
+			var toReturn = new BsonDocument {
+				{"message", ex.Message}, 
+				{"source", ex.Source}, 
+				{"stackTrace", ex.StackTrace}
+			};
 
 			if(ex.InnerException != null)
 			{
-				toReturn["innerException"] = BuildExceptionBsonDocument(ex.InnerException);
+				toReturn.Add("innerException", BuildExceptionBsonDocument(ex.InnerException));
 			}
 
 			return toReturn;
