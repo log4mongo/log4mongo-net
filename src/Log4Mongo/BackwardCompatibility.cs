8295504cb759bdd9816fb6be2d40cf5bf08fdafa
ryan anderson <ryan.anderson@buzzuti.com>
2014-06-04 16:55:48 -0500

    Updated NUnit to 2.6.3, updated the mongocharpdriver to 1.9.1.

--- src/Log4Mongo/BackwardCompatibility.cs
+++ src/Log4Mongo/BackwardCompatibility.cs
@@ -8,31 +8,31 @@ using log4net.Util;
 
 namespace Log4Mongo
 {
-	public class BackwardCompatibility
-	{
-		public static MongoDatabase GetDatabase(MongoDBAppender appender)
-		{
-			var port = appender.Port > 0 ? appender.Port : 27017;
-			var mongoConnectionString = new StringBuilder(string.Format("Server={0}:{1}", appender.Host ?? "localhost", port));
-			if(!string.IsNullOrEmpty(appender.UserName) && !string.IsNullOrEmpty(appender.Password))
-			{
-				// use MongoDB authentication
-				mongoConnectionString.AppendFormat(";Username={0};Password={1}", appender.UserName, appender.Password);
-			}
+    public class BackwardCompatibility
+    {
+        public static MongoDatabase GetDatabase(MongoDBAppender appender)
+        {
+            var port = appender.Port > 0 ? appender.Port : 27017;
+            var mongoConnectionString = new StringBuilder(string.Format("Server={0}:{1}", appender.Host ?? "localhost", port));
+            if (!string.IsNullOrEmpty(appender.UserName) && !string.IsNullOrEmpty(appender.Password))
+            {
+                // use MongoDB authentication
+                mongoConnectionString.AppendFormat(";Username={0};Password={1}", appender.UserName, appender.Password);
+            }
 
-			MongoServer connection = MongoServer.Create(mongoConnectionString.ToString()); // TODO Should be replaced with MongoClient, but this will change default for WriteConcern. See http://blog.mongodb.org/post/36666163412/introducing-mongoclient and http://docs.mongodb.org/manual/release-notes/drivers-write-concern
-			connection.Connect();
-			return connection.GetDatabase(appender.DatabaseName ?? "log4net_mongodb");
-		}
+            MongoServer connection = MongoServer.Create(mongoConnectionString.ToString()); // TODO Should be replaced with MongoClient, but this will change default for WriteConcern. See http://blog.mongodb.org/post/36666163412/introducing-mongoclient and http://docs.mongodb.org/manual/release-notes/drivers-write-concern
+            connection.Connect();
+            return connection.GetDatabase(appender.DatabaseName ?? "log4net_mongodb");
+        }
 
-		public static BsonDocument BuildBsonDocument(LoggingEvent loggingEvent)
-		{
-			if(loggingEvent == null)
-			{
-				return null;
-			}
+        public static BsonDocument BuildBsonDocument(LoggingEvent loggingEvent)
+        {
+            if (loggingEvent == null)
+            {
+                return null;
+            }
 
-			var toReturn = new BsonDocument {
+            var toReturn = new BsonDocument {
 				{"timestamp", loggingEvent.TimeStamp}, 
 				{"level", loggingEvent.Level.ToString()}, 
 				{"thread", loggingEvent.ThreadName}, 
@@ -43,51 +43,53 @@ namespace Log4Mongo
 				{"machineName", Environment.MachineName}
 			};
 
-			// location information, if available
-			if(loggingEvent.LocationInformation != null)
-			{
-				toReturn.Add("fileName", loggingEvent.LocationInformation.FileName);
-				toReturn.Add("method", loggingEvent.LocationInformation.MethodName);
-				toReturn.Add("lineNumber", loggingEvent.LocationInformation.LineNumber);
-				toReturn.Add("className", loggingEvent.LocationInformation.ClassName);
-			}
+            // location information, if available
+            if (loggingEvent.LocationInformation != null)
+            {
+                toReturn.Add("fileName", loggingEvent.LocationInformation.FileName);
+                toReturn.Add("method", loggingEvent.LocationInformation.MethodName);
+                toReturn.Add("lineNumber", loggingEvent.LocationInformation.LineNumber);
+                toReturn.Add("className", loggingEvent.LocationInformation.ClassName);
+            }
 
-			// exception information
-			if(loggingEvent.ExceptionObject != null)
-			{
-				toReturn.Add("exception", BuildExceptionBsonDocument(loggingEvent.ExceptionObject));
-			}
+            // exception information
+            if (loggingEvent.ExceptionObject != null)
+            {
+                toReturn.Add("exception", BuildExceptionBsonDocument(loggingEvent.ExceptionObject));
+            }
 
-			// properties
-			PropertiesDictionary compositeProperties = loggingEvent.GetProperties();
-		    if (compositeProperties == null || compositeProperties.Count <= 0) 
+            // properties
+            PropertiesDictionary compositeProperties = loggingEvent.GetProperties();
+            if (compositeProperties == null || compositeProperties.Count <= 0)
                 return toReturn;
-		    
+
             var properties = new BsonDocument();
-		    foreach(DictionaryEntry entry in compositeProperties)
-		    {
-		        properties.Add(entry.Key.ToString().Replace("log4net:", ""), entry.Value.ToString());
-		    }
+            foreach (DictionaryEntry entry in compositeProperties)
+            {
+                BsonValue value;
+                properties.Add(entry.Key.ToString().Replace("log4net:", ""),
+                    !BsonTypeMapper.TryMapToBsonValue(entry.Value, out value) ? entry.Value.ToBsonDocument() : value);
+            }
 
-		    toReturn.Add("properties", properties);
+            toReturn.Add("properties", properties);
 
-		    return toReturn;
-		}
+            return toReturn;
+        }
 
-		private static BsonDocument BuildExceptionBsonDocument(Exception ex)
-		{
-			var toReturn = new BsonDocument {
+        private static BsonDocument BuildExceptionBsonDocument(Exception ex)
+        {
+            var toReturn = new BsonDocument {
 				{"message", ex.Message}, 
 				{"source", ex.Source}, 
 				{"stackTrace", ex.StackTrace}
 			};
 
-			if(ex.InnerException != null)
-			{
-				toReturn.Add("innerException", BuildExceptionBsonDocument(ex.InnerException));
-			}
+            if (ex.InnerException != null)
+            {
+                toReturn.Add("innerException", BuildExceptionBsonDocument(ex.InnerException));
+            }
 
-			return toReturn;
-		}
-	}
+            return toReturn;
+        }
+    }
 }
