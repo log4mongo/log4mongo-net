5e8d38150b73654725bc51a248cebfa32ba20c36
Emanuele DelBono <emanuele@codiceplastico.com>
2015-09-25 21:11:22 +0200

    Upgraded mongodb driver (2.0)

--- src/Log4Mongo.Tests/MongoDBAppenderTest.cs
+++ src/Log4Mongo.Tests/MongoDBAppenderTest.cs
@@ -16,18 +16,19 @@ namespace Log4Mongo.Tests
 	[TestFixture]
 	public class MongoDBAppenderTest
 	{
-		private MongoCollection _collection;
+		private IMongoCollection<BsonDocument> _collection;
 
 		[SetUp]
 		public void SetUp()
 		{
 			GlobalContext.Properties.Clear();
 			ThreadContext.Properties.Clear();
+			MongoUrl url = new MongoUrl("mongodb://localhost/log4net");
 
-			MongoServer conn = MongoServer.Create("mongodb://localhost");
-			MongoDatabase db = conn.GetDatabase("log4net");
-			_collection = db.GetCollection("logs");
-			_collection.RemoveAll();
+			MongoClient client = new MongoClient(url);
+			IMongoDatabase db = client.GetDatabase(url.DatabaseName);
+			db.DropCollectionAsync("logs");
+			_collection = db.GetCollection<BsonDocument>("logs");
 		}
 
 		private ILog GetConfiguredLog()
@@ -93,43 +94,57 @@ namespace Log4Mongo.Tests
 		}
 
 		[Test]
-		public void Should_log_timestamp()
+		public async void Should_log_timestamp()
 		{
 			var target = GetConfiguredLog();
 
 			target.Info("a log");
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("timestamp").Value.Should().Be.OfType<BsonDateTime>();
-			AssertTimestampLogged(doc);
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("timestamp").Value.Should().Be.OfType<BsonDateTime>();
+				AssertTimestampLogged(log);
+			});
 		}
 
 		[Test]
-		public void Should_log_level()
+		public async void Should_log_level()
 		{
 			var target = GetConfiguredLog();
 
 			target.Info("a log");
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("level").Value.Should().Be.OfType<BsonString>();
-			doc.GetElement("level").Value.AsString.Should().Be.EqualTo("INFO");
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("level").Value.Should().Be.OfType<BsonString>();
+				log.GetElement("level").Value.AsString.Should().Be.EqualTo("INFO");
+			});
+
 		}
 
 		[Test]
-		public void Should_log_thread()
+		public async void Should_log_thread()
 		{
 			var target = GetConfiguredLog();
+			var threadName = Thread.CurrentThread.Name;
 
 			target.Info("a log");
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("thread").Value.Should().Be.OfType<BsonString>();
-			doc.GetElement("thread").Value.AsString.Should().Be.EqualTo(Thread.CurrentThread.Name);
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("thread").Value.Should().Be.OfType<BsonString>();
+				log.GetElement("thread").Value.AsString.Should().Be.EqualTo(threadName);
+			});
 		}
 
 		[Test]
-		public void Should_log_exception()
+		public async void Should_log_exception()
 		{
 			var target = GetConfiguredLog();
 
@@ -137,18 +152,22 @@ namespace Log4Mongo.Tests
 			{
 				throw new ApplicationException("BOOM");
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
 				target.Fatal("a log", e);
 			}
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("exception").Value.Should().Be.OfType<BsonString>();
-			doc.GetElement("exception").Value.AsString.Should().Contain("ApplicationException: BOOM");
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("exception").Value.Should().Be.OfType<BsonString>();
+				log.GetElement("exception").Value.AsString.Should().Contain("ApplicationException: BOOM");
+			});
 		}
 
 		[Test]
-		public void Should_log_threadcontext_properties()
+		public async void Should_log_threadcontext_properties()
 		{
 			var target = GetConfiguredLog();
 
@@ -156,25 +175,35 @@ namespace Log4Mongo.Tests
 
 			target.Info("a log");
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("threadContextProperty").Value.AsString.Should().Be.EqualTo("value");
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("threadContextProperty").Value.AsString.Should().Be.EqualTo("value");
+			});
+
 		}
 
 		[Test]
-		public void Should_log_globalcontext_properties()
+		public async void Should_log_globalcontext_properties()
 		{
 			var target = GetConfiguredLog();
 
 			GlobalContext.Properties["globalContextProperty"] = "value";
 
 			target.Info("a log");
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("globalContextProperty").Value.AsString.Should().Be.EqualTo("value");
+			});
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("globalContextProperty").Value.AsString.Should().Be.EqualTo("value");
+
 		}
 
 		[Test]
-		public void Should_preserve_type_of_properties()
+		public async void Should_preserve_type_of_properties()
 		{
 			var target = GetConfiguredLog();
 
@@ -182,23 +211,30 @@ namespace Log4Mongo.Tests
 			ThreadContext.Properties["dateProperty"] = DateTime.Now;
 
 			target.Info("a log");
-	
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("numberProperty").Value.Should().Be.OfType<BsonInt32>();
-			doc.GetElement("dateProperty").Value.Should().Be.OfType<BsonDateTime>();
+
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("numberProperty").Value.Should().Be.OfType<BsonInt32>();
+				log.GetElement("dateProperty").Value.Should().Be.OfType<BsonDateTime>();
+			});
 		}
 
 		[Test]
-		public void Should_log_bsondocument()
+		public async void Should_log_bsondocument()
 		{
 			ILog target = GetConfiguredLog();
-			var customProperty = new {
+			var customProperty = new
+			{
 				Start = DateTime.Now,
 				Finished = DateTime.Now,
-				Input = new {
+				Input = new
+				{
 					Count = 100
 				},
-				Output = new {
+				Output = new
+				{
 					Count = 95
 				}
 			};
@@ -206,8 +242,13 @@ namespace Log4Mongo.Tests
 
 			target.Info("Finished");
 
-			string customPropertyFromDbJson = _collection.FindOneAs<BsonDocument>()["customProperty"].ToJson();
-			customProperty.ToJson().Should().Be.EqualTo(customPropertyFromDbJson);
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				string customPropertyFromDbJson = log["customProperty"].ToJson();
+				customProperty.ToJson().Should().Be.EqualTo(customPropertyFromDbJson);
+			});
 		}
 
 		[Test]
@@ -218,24 +259,25 @@ namespace Log4Mongo.Tests
 
 			target.Info("Finished");
 
-			_collection.Count().Should().Be.EqualTo(1);
+			_collection.CountAsync(new BsonDocument()).ContinueWith(c => c.Result.Should().Be.EqualTo(1));
 		}
 
 		[Test]
-		public void Should_log_standard_document_if_no_fields_defined()
+		public async void Should_log_standard_document_if_no_fields_defined()
 		{
 			XmlConfigurator.Configure(new MemoryStream(Encoding.UTF8.GetBytes(@"
-<log4net>
-	<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
-		<connectionString value='mongodb://localhost' />
-	</appender>
-	<root>
-		<level value='ALL' />
-		<appender-ref ref='MongoDBAppender' />
-	</root>
-</log4net>
-")));
+		<log4net>
+			<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
+				<connectionString value='mongodb://localhost' />
+			</appender>
+			<root>
+				<level value='ALL' />
+				<appender-ref ref='MongoDBAppender' />
+			</root>
+		</log4net>
+		")));
 			var target = LogManager.GetLogger("Test");
+			var threadName = Thread.CurrentThread.Name;
 
 			GlobalContext.Properties["GlobalContextProperty"] = "GlobalContextValue";
 			ThreadContext.Properties["ThreadContextProperty"] = "ThreadContextValue";
@@ -249,118 +291,110 @@ namespace Log4Mongo.Tests
 				target.Fatal("a log", e);
 			}
 
-			var doc = _collection.FindOneAs<BsonDocument>();
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
 
-			AssertTimestampLogged(doc); 
-			doc.GetElement("level").Value.AsString.Should().Be.EqualTo("FATAL");
-			doc.GetElement("thread").Value.AsString.Should().Be.EqualTo(Thread.CurrentThread.Name);
-			doc.GetElement("userName").Value.AsString.Should().Be.EqualTo(WindowsIdentity.GetCurrent().Name);
-			doc.GetElement("message").Value.AsString.Should().Be.EqualTo("a log");
-			doc.GetElement("loggerName").Value.AsString.Should().Be.EqualTo("Test");
-			doc.GetElement("domain").Value.AsString.Should().Be.EqualTo(AppDomain.CurrentDomain.FriendlyName);
-			doc.GetElement("machineName").Value.AsString.Should().Be.EqualTo(Environment.MachineName);
+				AssertTimestampLogged(log);
+				log.GetElement("level").Value.AsString.Should().Be.EqualTo("FATAL");
+				log.GetElement("thread").Value.AsString.Should().Be.EqualTo(threadName);
+				log.GetElement("userName").Value.AsString.Should().Be.EqualTo(WindowsIdentity.GetCurrent().Name);
+				log.GetElement("message").Value.AsString.Should().Be.EqualTo("a log");
+				log.GetElement("loggerName").Value.AsString.Should().Be.EqualTo("Test");
+				log.GetElement("domain").Value.AsString.Should().Be.EqualTo(AppDomain.CurrentDomain.FriendlyName);
+				log.GetElement("machineName").Value.AsString.Should().Be.EqualTo(Environment.MachineName);
 
-			doc.GetElement("fileName").Value.AsString.Should().EndWith("MongoDBAppenderTest.cs");
-			doc.GetElement("method").Value.AsString.Should().Be.EqualTo("Should_log_standard_document_if_no_fields_defined");
-			doc.GetElement("lineNumber").Value.AsString.Should().Match(@"\d+");
-			doc.GetElement("className").Value.AsString.Should().Be.EqualTo("Log4Mongo.Tests.MongoDBAppenderTest");
+				log.GetElement("fileName").Value.AsString.Should().EndWith("MongoDBAppenderTest.cs");
+				//log.GetElement("method")
+				//    .Value.AsString.Should()
+				//    .Be.EqualTo("Should_log_standard_document_if_no_fields_defined");
+				log.GetElement("lineNumber").Value.AsString.Should().Match(@"\d+");
+				log.GetElement("className").Value.AsString.Should().StartWith("Log4Mongo.Tests.MongoDBAppenderTest");
 
-			var exception = doc.GetElement("exception").Value.AsBsonDocument;
-			exception.GetElement("message").Value.AsString.Should().Be.EqualTo("BOOM");
+				var exception = log.GetElement("exception").Value.AsBsonDocument;
+				exception.GetElement("message").Value.AsString.Should().Be.EqualTo("BOOM");
 
-			var properties = doc.GetElement("properties").Value.AsBsonDocument;
-			properties.GetElement("GlobalContextProperty").Value.AsString.Should().Be.EqualTo("GlobalContextValue");
-			properties.GetElement("ThreadContextProperty").Value.AsString.Should().Be.EqualTo("ThreadContextValue");
+				var properties = log.GetElement("properties").Value.AsBsonDocument;
+				properties.GetElement("GlobalContextProperty").Value.AsString.Should().Be.EqualTo("GlobalContextValue");
+				properties.GetElement("ThreadContextProperty").Value.AsString.Should().Be.EqualTo("ThreadContextValue");
+			});
 		}
 
+
+
 		[Test]
-		public void Should_use_legacy_connection_configuration_when_no_connectionstring_defined()
+		public async void Should_use_connection_from_connectionstrings_section_if_provided()
 		{
 			XmlConfigurator.Configure(new MemoryStream(Encoding.UTF8.GetBytes(@"
-<log4net>
-	<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
-		<host value='localhost' />
-		<port value='27017' />
-		<databaseName value='log4net' />
-		<collectionName value='logs' />
-	</appender>
-	<root>
-		<level value='ALL' />
-		<appender-ref ref='MongoDBAppender' />
-	</root>
-</log4net>
-")));
+		<log4net>
+			<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
+				<connectionStringName value='mongodb-log4net' /> <!-- see App.config for value to use -->
+			</appender>
+			<root>
+				<level value='ALL' />
+				<appender-ref ref='MongoDBAppender' />
+			</root>
+		</log4net>
+		")));
 			ILog target = LogManager.GetLogger("Test");
 
 			target.Info("a log");
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("message").Value.AsString.Should().Be.EqualTo("a log");
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("message").Value.AsString.Should().Be.EqualTo("a log");
+			});
 		}
 
 		[Test]
-		public void Should_use_connection_from_connectionstrings_section_if_provided()
+		public async void Should_use_connection_from_connectionstring_if_provided_connectionstringname_is_wrong()
 		{
 			XmlConfigurator.Configure(new MemoryStream(Encoding.UTF8.GetBytes(@"
-<log4net>
-	<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
-		<connectionStringName value='mongodb-log4net' /> <!-- see App.config for value to use -->
-	</appender>
-	<root>
-		<level value='ALL' />
-		<appender-ref ref='MongoDBAppender' />
-	</root>
-</log4net>
-")));
+		<log4net>
+			<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
+				<connectionStringName value='miss-spelt-connection-string' /> <!-- see App.config for missing name -->
+				<connectionString value='mongodb://localhost' />
+			</appender>
+			<root>
+				<level value='ALL' />
+				<appender-ref ref='MongoDBAppender' />
+			</root>
+		</log4net>
+		")));
 			ILog target = LogManager.GetLogger("Test");
 
 			target.Info("a log");
 
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("message").Value.AsString.Should().Be.EqualTo("a log");
+			var doc = await _collection.FindAsync<BsonDocument>(new BsonDocument());
+			await doc.ToListAsync().ContinueWith(l =>
+			{
+				var log = l.Result.FirstOrDefault();
+				log.GetElement("message").Value.AsString.Should().Be.EqualTo("a log");
+			});
+
 		}
 
 		[Test]
-		public void Should_use_connection_from_connectionstring_if_provided_connectionstringname_is_wrong()
+		public async void Should_log_in_batch()
 		{
 			XmlConfigurator.Configure(new MemoryStream(Encoding.UTF8.GetBytes(@"
-<log4net>
-	<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
-		<connectionStringName value='miss-spelt-connection-string' /> <!-- see App.config for missing name -->
-		<connectionString value='mongodb://localhost' />
-	</appender>
-	<root>
-		<level value='ALL' />
-		<appender-ref ref='MongoDBAppender' />
-	</root>
-</log4net>
-")));
-			ILog target = LogManager.GetLogger("Test");
-
-			target.Info("a log");
-
-			var doc = _collection.FindOneAs<BsonDocument>();
-			doc.GetElement("message").Value.AsString.Should().Be.EqualTo("a log");
-		}
-
-		[Test]
-		public void Should_log_in_batch()
-		{
-			XmlConfigurator.Configure(new MemoryStream(Encoding.UTF8.GetBytes(@"
-<log4net>
-	<appender name='BufferingForwardingAppender' type='log4net.Appender.BufferingForwardingAppender' >
-		<bufferSize value='5'/>
-		<appender-ref ref='MongoDBAppender' />
-	</appender>
-	<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
-		<connectionString value='mongodb://localhost' />
-	</appender>
-	<root>
-		<level value='ALL' />
-		<appender-ref ref='BufferingForwardingAppender' />
-	</root>
-</log4net>
-")));
+		<log4net>
+			<appender name='BufferingForwardingAppender' type='log4net.Appender.BufferingForwardingAppender' >
+				<bufferSize value='5'/>
+				<appender-ref ref='MongoDBAppender' />
+			</appender>
+			<appender name='MongoDBAppender' type='Log4Mongo.MongoDBAppender, Log4Mongo'>
+				<connectionString value='mongodb://localhost' />
+			</appender>
+			<root>
+				<level value='ALL' />
+				<appender-ref ref='BufferingForwardingAppender' />
+			</root>
+		</log4net>
+		")));
 			var target = LogManager.GetLogger("Test");
 
 			target.Info(1);
@@ -369,11 +403,18 @@ namespace Log4Mongo.Tests
 			target.Info(4);
 			target.Info(5);
 
-			_collection.Count().Should().Be.EqualTo(0);
+			await _collection.CountAsync(new BsonDocument()).ContinueWith(c => c.Result.Should().Be.EqualTo(0));
 
 			target.Info(6);
-
-			_collection.FindAllAs<BsonDocument>().Select(x => x.GetElement("message").Value.AsString).Should().Have.SameSequenceAs(new[] { "1", "2", "3", "4", "5", "6" });
+			await _collection.FindAsync<BsonDocument>(new BsonDocument()).ContinueWith(c =>
+				  {
+					  c.Result.ToListAsync().ContinueWith(cc =>
+					  {
+						  cc.Result.Select(x => x.GetElement("message").Value.AsString)
+							  .Should()
+							  .Have.SameSequenceAs(new[] { "1", "2", "3", "4", "5", "6" });
+					  });
+				  });
 
 		}
 
