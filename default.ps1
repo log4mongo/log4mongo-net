44dd7044983123b6a35f285bea76b585be088f7f
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2019-02-04 17:11:52 +0100

    Updated build to use new dotnet tools

--- default.ps1
+++ default.ps1
@@ -9,37 +9,17 @@ Task default -Depends Test
 
 Task Deps {
 	Exec { & "nuget.exe" install NUnit.Runners -Version "2.6.0.12051" -ExcludeVersion -OutputDirectory "$build_dir\tools" }
-    
-    Get-ChildItem "$build_dir\src\*\packages.config" -Exclude .nuget | ForEach-Object {
-    	Write-Host "Downloading packages defined in $_"
-    	Exec { & "nuget.exe" install $_ -OutputDirectory "$build_dir\lib" }
-    }
 }
 
-Task AssemblyInfo {
+Task Compile -Depends Deps {
     $version = Get-Content $build_dir\version.txt
-    $year = (Get-Date).Year
-    @(
-        "[assembly: System.Reflection.AssemblyTitle(`"Log4Mongo`")]",
-        "[assembly: System.Reflection.AssemblyProduct(`"Log4Mongo`")]",
-        "[assembly: System.Reflection.AssemblyDescription(`"MongoDB appender for log4net`")]",
-        "[assembly: System.Reflection.AssemblyCopyright(`"Copyright © Gian Marco Gherardi $year`")]",
-        "[assembly: System.Reflection.AssemblyTrademark(`"`")]",
-        "[assembly: System.Reflection.AssemblyCompany(`"Gian Marco Gherardi`")]",
-        "[assembly: System.Reflection.AssemblyConfiguration(`"$build_cfg`")]",
-        "[assembly: System.Reflection.AssemblyVersion(`"$version.0`")]",
-        "[assembly: System.Reflection.AssemblyFileVersion(`"$version.0`")]",
-        "[assembly: System.Reflection.AssemblyInformationalVersion(`"$version`")]"
-    ) | Out-File "$build_dir\src\SharedAssemblyInfo.cs" -Encoding 'utf8'
-}
+    $copyright = "Gian Marco Gherardi " + (Get-Date).Year
 
-Task Compile -Depends Deps, AssemblyInfo {
-    Write-Host "Compiling in $build_cfg configuration"
-	Exec { msbuild "$build_dir\src\Log4Mongo.sln" /t:Build /p:Configuration=$build_cfg /v:quiet /nologo }
+	Exec { dotnet pack --configuration $build_cfg -p:Version=$version "-p:Copyright=$copyright" "$build_dir\src\Log4Mongo.sln" }
 }
 
 Task Test -Depends Compile {
-    $TestDlls = Get-ChildItem "$build_dir\src\*\bin\$build_cfg\*.Tests.dll"
+    $TestDlls = Get-ChildItem "$build_dir\src\*\bin\$build_cfg\net45\publish\*.Tests.dll"
     Exec { & "$build_dir\tools\NUnit.Runners\tools\nunit-console.exe" /nologo /noresult /framework=4.0.30319 @TestDlls }
 }
 
@@ -49,7 +29,6 @@ Task Publish -Depends Test {
     New-Item $build_dir\build -type directory
 
    	Exec { & "nuget.exe" pack "$build_dir\src\Log4Mongo\Log4Mongo.csproj" -Build -OutputDirectory $build_dir\build, -Symbols -Prop Configuration=$build_cfg }
-	Exec { & "nuget.exe" push "$build_dir\build\log4mongo-net.$version.nupkg" }
 
     $version = $version -split "\."
     $version[2] =  [System.Int32]::Parse($version[2]) + 1
--- src/Log4Mongo/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("b7b971cf-3b25-4889-99cc-62fdd6fecaa4")]
